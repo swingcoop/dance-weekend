580bf2d135297e79c1302dd8b7e97cd957ab76ee
Philip Manijak <github@morningshift.org>
2019-09-29 23:39:26 -0700

    www: Add period to Let's go.

--- www/src/views/Home.vue
+++ www/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img src="../assets/corvallis-swing.svg" width="33%"/>
-    <h1>Year 10. Let's go</h1>
+    <h1>Year 10. Let's go.</h1>
 
     <router-link to="/rsvp">R.S.V.P.</router-link>
   </div>
