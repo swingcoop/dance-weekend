0fc2cea03537a54bb2bc66cd66bda729f280f8a6
Philip Manijak <github@morningshift.org>
2019-12-05 21:27:26 -0800

    www: Add bands to Home

--- www/src/views/Home.vue
+++ www/src/views/Home.vue
@@ -3,9 +3,45 @@
     <div class="logo-wrapper">
       <CorvallisSwingLogo class="logo"/>
     </div>
+    <div class="rsvp">
+      <router-link to="/rsvp">R.S.V.P.</router-link>
+    </div>
     <h1>Year 10. Let's go.</h1>
 
-    <router-link to="/rsvp">R.S.V.P.</router-link>
+    <h2>Featuring</h2>
+
+    <div class="feature">
+      <img src="../assets/jen.jpg" width="100%"/>
+      <h2>Jen Hodge</h2>
+      <h4>New York City</h4>
+    </div>
+    
+    <div class="feature">
+      <img src="../assets/cherry-blossom.jpg" width="100%"/>
+      <h2>Cherry Blossom Orchestra</h2>
+      <h4>Portland</h4>
+    </div>
+
+    <div class="feature">
+      <img src="../assets/breakers-yard.jpg" width="100%"/>
+      <h2>Breakers Yard</h2>
+      <h4>Eugene</h4>
+    </div>
+
+    <div class="feature">
+      <img src="../assets/stealin-apples.jpg" width="100%"/>
+      <h2>Stealin' Apples Jazz Band</h2>
+      <h4>Eugene</h4>
+    </div>
+
+    <div class="event feature">
+      <h1>Jan 31 &ndash; Feb 2, 2020</h1>
+      <h2>Corvallis, Oregon</h2>
+    </div>
+
+    <div class="rsvp">
+      <router-link to="/rsvp">R.S.V.P.</router-link>
+    </div>
   </div>
 </template>
 
@@ -27,6 +63,9 @@ export default {
   body {
     color: white;
   }
+  .rsvp {
+    margin-top: 1em;
+  }
   .home {
     text-align: center;
   }
@@ -36,5 +75,21 @@ export default {
   .logo-wrapper {
     width: 33%;
     display: inline-block;
+  }
+  .feature h1 {
+    margin-bottom: 0;
+  }
+  .feature h2 {
+    margin-top: 0.6em;
+    margin-bottom: 0;
+  }
+  .feature h4 {
+    margin-top: 0.5ex;
+  }
+  .feature img {
+    margin-top: 2em;
+  }
+  .event {
+    margin-top: 5em;
   }
 </style>
