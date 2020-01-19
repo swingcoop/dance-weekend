72212dab5beba6cc0f5b5664ce466f60860eda31
Philip Manijak <github@morningshift.org>
2020-01-19 12:05:18 -0800

    www: Add detailed schedule

--- www/src/views/Schedule.vue
+++ www/src/views/Schedule.vue
@@ -2,7 +2,202 @@
 <div>
     <h1>Three nights.<br/>Dancing 'til 3.</h1>
 
-    <h2>Friday, January 31.</h2>
+    <section>
+        <h2>Friday, January 31</h2>
+        <div class="venue">
+            Odd Fellows Hall. 223 SW 2nd St.
+        </div>
+
+        <h3 class="time start">7:30 p.m.</h3>
+        <p>Doors open. Please check in at the front desk.</p>
+
+        <h3 class="time">8:00 p.m.</h3>
+        <p>Intro lesson featuring the Charleston, lead by Lindsey Almarode and Bailey Keefe.</p>
+
+        <h3 class="time">8:30 p.m.</h3>
+
+        <h4>Cherry Blossom Hot 5</h4>
+        <p>
+            The Cherry Blossom Hot 5 have been playing for swing dancers regularly
+            for over two years and continue to broaden their popularity in the scenes
+            in Washington as well as here in Oregon.
+        </p> 
+        
+        <img src="../assets/cherry-blossom.jpg" width="100%"/>
+
+        <p>
+            Led by an experienced swing dancer and prolific DJ, the band has a unique 
+            musical understanding of the ingredients required for the best night's dancing, 
+            with a mix of tunes and styles through the 1920s to the 1940s including some 
+            of the more iconic renditions of tunes dancers love for the popular choreographed 
+            group dances like the Shim Sham and Tranky Doo, and the meaningful tunes such as
+            those much loved by the late and great Frankie Manning and Norma Miller.
+        </p>
+
+        <p>
+            The musicians in the band are stars in their own right, the very top on their 
+            instrument, with a long tradition in the style.
+        </p>
+        
+        <p>
+            All in all, an exceptional blend to celebrate the 100th anniversary of Prohibition!
+        </p>
+
+        <!-- <div class="time-panel">
+            <div class="open">
+                <h3>Doors</h3>
+                <div>7:30 p.m.</div>
+            </div>
+
+            <div class="lesson">
+                <h3>Lesson</h3>
+                8 - 8:30 p.m. 
+            </div>
+
+            <div class="dance">
+                <h3>Dance</h3>
+                8:30 - 11:30 p.m.
+            </div>
+        </div> -->
+
+        <h2>Friday Late Night</h2>
+        <div class="venue">
+            Odd Fellows Hall. 223 SW 2nd St.
+        </div>
+
+        <h3 class="time">11:30 p.m.</h3>
+        <p>Snack time, with DJ Kristin Buxton.</p>
+        
+        <h3 class="time">12:30 a.m.</h3>
+        <h4>Stealin' Apples Jazz Band<br/>featuring Jen Hodge</h4>
+        <p>
+            The Stealin’ Apples Jazz Band is pianist Shane Allen’s love letter to classic, 
+            rowdy, four-on-the-floor swing dance music. From a young age Shane has been a
+            gigging musician and dedicated student of the jazz tradition, blessed with
+            the opportunity to learn from modern giants as well as classic masters. 
+        </p>
+
+        <img src="../assets/stealin-apples.jpg" width="100%"/>
+
+        <p>
+            After years working as a dance accompanist, playing Lindy hop house parties in 
+            Minneapolis, and haunting the late night jams at Herräng Dance Camp, he formed
+            the Stealin’ Apples Jazz Band to connect the world-class faculty and students
+            of the University of Oregon jazz program with lindy hoppers’ passion for authentic
+            swing-era dance music.
+        </p>
+
+        <h3 class="time">3:00 a.m.</h3>
+        <p>End of the first night.</p>
+    </section>
+
+    <section class="saturday">
+        <h2>Saturday Afternoon</h2>
+        <div class="venue">
+            Imagine Coffee. 5460 Philomath Blvd.
+        </div>
+        <h3 class="time">1:00 p.m.</h3>    
+        <p>
+            Day two begins with an open band jam, featuring local musicians 
+            as well as those in 
+            town for the event, playing a range of jazz, funk, blues 
+            and whatever comes out of this casual get-together.
+        </p>
+
+        <h3 class="time">3:00 p.m.</h3>
+    </section>
+
+    <section class="saturday">
+        <h2>Saturday Evening</h2>
+        <div class="venue">
+            Odd Fellows Hall. 223 SW 2nd St.
+        </div>
+
+        <h3 class="time">7:00 p.m.</h3>
+        <p>Intro lesson featuring 6-count Lindy hop, lead by Nika Jin and Nick Davis.</p>
+
+        <h3 class="time">8:00 p.m.</h3>
+        <h4>Stealin' Apples Jazz Band<br/>featuring Jen Hodge</h4>
+        <p>
+            Busy PNW-bred/NYC-based bass player, vocalist, bandleader, and arranger 
+            Jen Hodge is a versatile musician who can be found playing with a wide
+            variety of artists all over the world.
+        </p>
+
+        <img src="../assets/jen.jpg" width="100%"/>
+        
+        <p>
+            Jen's playing is characterized by hard swinging grooves, an expressive 
+            and energetic playing style, and an evident, infectious love for making music. 
+            Her steadfast bass playing and consummate professionalism have taken her to 
+            stages all over Canada, the USA, China, Spain, France, the UK, Sweden, and 
+            the Caribbean. Increasingly in demand in the global swing dance community, Jen has appeared
+            at such notable events as Herräng, Lindy Focus, and Camp Jitterbug. 
+        </p> 
+        
+        <p>
+            Jen's 2018 album, All's Fair in Love and Jazz, spent 8 weeks in the top 30 
+            North American College &amp; Community Radio jazz charts, during which 
+            time it was one of the highest charting self-released titles; 
+            peaked at #2 in the !earshot Canadian jazz charts; 
+            and broke the top 100 in the commercial JazzWeek charts.
+        </p>
+
+        <h2>Saturday Late Night</h2>
+        <div class="venue">
+            Odd Fellows Hall. 223 SW 2nd St.
+        </div>
+
+        <h3 class="time">11:00 p.m.</h3>
+        <p>Snack time, with DJ Nick Davis.</p>
+
+        <h3 class="time">Midnight</h3>
+        <h4>Breakers Yard</h4>
+        <p>
+            Breakers Yard is the Willamette Valley’s most eclectic acoustic band. 
+            Sampling from ragtime, country blues, jug band, hot jazz, and delta blues traditions, 
+            they do everything from Lindy hop dances to cowboy ballads to bootie-shakin’ 
+            dance parties.             
+        </p>
+
+        <img src="../assets/breakers-yard.jpg" width="100%"/>
+
+        <p>
+            You can find their most recent albums I Don’t Hurt Anymore and Tried and 
+            Untrue at shows, on Spotify, and on CD Baby, or you can pay them a visit 
+            at <a href="http://www.breakersyardmusic.com">www.breakersyardmusic.com</a>. 
+        </p>
+
+        <p>Break it down!</p>
+
+        <h3 class="time">3:00 a.m.</h3>
+        <p>End of the second night.</p>
+    </section>
+
+    <section class="sunday">
+        <h2>Sunday Afternoon</h2>
+        <div class="venue">
+            Old World Deli. 341 SW 2nd St.
+        </div>
+
+        <h3 class="time">2:00 p.m.</h3>
+        <p>Casual afternoon dance.</p>
+        <h3 class="time">4:00 p.m.</h3>
+    </section>
+
+    <section class="sunday">
+        <h2>Sunday Evening</h2>
+        <div class="venue">
+            OSU Women’s Building. 160 SW 26th St.
+        </div>
+        <h3 class="time">8:00 p.m.</h3>
+        
+        <p>The final dance, with past and present Corvallis Swing DJs.</p>
+        <h3 class="time">11:00 p.m.</h3>
+        <p>Fin.</p>
+    </section>
+
+    <!-- <h2>Friday, January 31.</h2>
     <div class="venue">Odd Fellows Hall</div>
     <div>8 p.m.</div>
     <div>Cherry Blossom Orchestra</div>
@@ -33,7 +228,7 @@
 
     <h2>Sunday evening.</h2>
     <div class="venue">OSU Women's Building</div>
-    <div>8 &ndash; 11 p.m.</div>
+    <div>8 &ndash; 11 p.m.</div> -->
 
     <div class="rsvp">
       <router-link to="/">Home</router-link>
@@ -47,11 +242,72 @@
 <style scoped>
 body {
     color: white;
+      /* margin-left: auto;
+  margin-right: auto; */
+  /* padding: 2ex; */
+}
+section {
+    padding-left: 2ex;
+    padding-right: 2ex;
+    padding-top: 0.5rem;
+    padding-bottom: 1rem;
+}
+h1 {
+    text-align: center;
 }
 h2 {
     padding-bottom: 0;
     margin-bottom: 0;
+    text-transform: uppercase;
+    text-align: center;
 }
+h4 {
+    text-transform: uppercase;
+    text-align: center;
+}
+
+/* h3 {
+    margin: 0;
+    width: 100%;
+    text-align: center;
+} */
+
+.time {
+	display: flex;
+	flex-basis: 100%;
+    justify-content: flex-start;
+	align-items: center;
+
+	/* color: rgba(0, 0, 0, 0.35); */
+	margin: 0px 0px;
+    
+}
+.time::before,
+.time::after {
+	content: "";
+	flex-grow: 1;
+	background: rgba(255, 255, 255, 0.35);
+	height: 2px;
+	font-size: 0;
+	line-height: 0;
+	margin: 0;
+}
+.time:before {
+    margin-right: 8px;
+    flex-grow: 1;
+}
+.time:after {
+    margin-left: 8px;
+    flex-grow: 0.05;
+}
+
+.start:before {
+    flex-grow: 1;
+}
+.start:after {
+    flex-grow: 0.05;   
+}
+
 .rsvp {
     margin-top: 3em;
     text-align: center;
@@ -59,5 +315,20 @@ h2 {
 .rsvp a {
     padding-left: 1ex;
     padding-right: 2ex;
+}
+.saturday {
+    background-color: #222;
+}
+.sunday {
+    background-color: #292929;
+}
+.time-panel {
+    display: flex; 
+    justify-content: space-between;
+}
+.venue {
+    margin-top: 0.4rem;
+    margin-bottom: 1.5rem;
+    text-align: center;
 }
 </style>
